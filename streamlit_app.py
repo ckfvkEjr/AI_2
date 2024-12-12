17fb766c0b26521144998610488fea537050c6cd
ckfvkEjr <cc.john2016@gmail.com>
2024-12-13 08:42:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,6 +173,14 @@ def main():
 
             with left_column:
                 display_left_content(mel_spec_path, pred, probs, learner.dls.vocab)
+            with right_column:
+                # 분류 결과에 따른 콘텐츠 선택
+                data = content_data.get(prediction, {
+                'images': ["https://via.placeholder.com/300"] * 3,
+                'videos': ["https://www.youtube.com/watch?v=3JZ_D3ELwOQ"] * 3,
+                'texts': ["기본 텍스트"] * 3
+                })
+                display_right_content(prediction, data)
 
 
 
