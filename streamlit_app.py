d00eaa96e935df01868d5e2e3b5a96a2c9d4e32b
ckfvkEjr <cc.john2016@gmail.com>
2024-12-13 08:39:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,13 +174,7 @@ def main():
             with left_column:
                 display_left_content(mel_spec_path, pred, probs, learner.dls.vocab)
 
-            with right_column:
-                data = content_data.get(pred, {
-                    'images': ["https://i.ibb.co/Gp5KgvV/memed-io-output.jpg"] * 3,
-                    'videos': ["https://www.youtube.com/watch?v=dQw4w9WgXcQ"] * 3,
-                    'texts': ["이거 어케 바꿔;;"] * 3
-                })
-                display_right_content(pred, data)
+
 
         except Exception as e:
             st.error(f"모델 예측 중 오류 발생: {e}")
